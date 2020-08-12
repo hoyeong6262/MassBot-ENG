76a2f6c362d9a05a5b4b140bc969e74fc71fe858
hoyeong6262 <69511294+hoyeong6262@users.noreply.github.com>
2020-08-12 13:39:03 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -12,7 +12,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("Your Bot is working.")
-    game = discord.Game('★★★Enter Game Name!!!!!!!★★★')
+    game = discord.Game('따라하기')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #Send dm to "/dm {message}"
@@ -25,13 +25,13 @@ async def on_message(message):
             else:
                 try:
                     msg = message.content[4:]
-                    if message.author.id == ★★★Enter Your Discord ID★★★:
-                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="★★Top Title★★")
-                        embed.add_field(name="★★Title★★", value=msg, inline=True)
+                    if message.author.id ==416903534160642049:
+                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="따라하기 게임중")
+                        embed.add_field(name="따라하기 게임중", value=msg, inline=True)
                         embed.set_footer(text=f"discord.gg/★★Invite Code★★")
                         await i.send(embed=embed)
                 except:
                     pass
 
 
-client.run('★★Bot Token★★')
+client.run('NzQyOTYxMjQyNTg2NDgwNjgw.XzNukw.Bmm4AR2tDzjSW4qCsXx7D1vwqhA')
